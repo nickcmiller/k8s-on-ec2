35e08bdf0a5c313d6f301de387a1b52ceb7a9213
Nick Miller <5348556+nickcmiller@users.noreply.github.com>
2023-01-01 17:44:23 -0800

    Update controlplanescript.sh

--- controlplanescript.sh
+++ controlplanescript.sh
@@ -1,18 +1,33 @@
-
-
 #Set up hostnames
-sudo hostnamectl set-hostname k8s-control
-sudo hostnamectl set-hostname k8s-worker1
-sudo hostnamectl set-hostname k8s-worker2
+# sudo hostnamectl set-hostname k8s-control
+# sudo hostnamectl set-hostname k8s-worker1
+# sudo hostnamectl set-hostname k8s-worker2
 
-#Set up hostfile
-cat << EOF >> /etc/hosts
-172.31.2.212 k8s-control
-172.31.8.96 k8s-worker1
-172.31.4.238 k8s-worker2
+#Set up hostfiles
+# cat << EOF >> /etc/hosts
+# 172.31.3.204 k8s-control
+# 172.31.59.55 k8s-worker1
+# 172.31.53.64 k8s-worker2
+# EOF
+
+# Kubeadm | kubectl | kubelet install
+curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+echo "deb https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list
+apt update -y
+apt -y install vim git curl wget kubelet=1.26.0-00 kubeadm=1.26.0-00 kubectl=1.26.0-00
+apt-mark hold kubelet kubeadm kubectl
+
+#Load the br_netfilter module and let iptables see bridged traffic
+modprobe overlay
+modprobe br_netfilter
+tee /etc/sysctl.d/kubernetes.conf<<EOF
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+net.ipv4.ip_forward = 1
 EOF
+sysctl --system
 
-# Create configuration fi
+# Create configuration files for Containerd
 cat <<EOF | sudo tee /etc/modules-load.d/containerd.conf
 overlay
 br_netfilter
@@ -22,7 +37,7 @@ EOF
 modprobe overlay
 modprobe br_netfilter
 
-#Networking level configurations
+# Setup required sysctl params, these persist across reboots.
 cat <<EOF | tee /etc/sysctl.d/99-kubernetes-cri.conf
 net.bridge.bridge-nf-call-ip6tables = 1
 net.bridge.bridge-nf-call-iptables = 1
@@ -32,41 +47,19 @@ EOF
 # Apply new settings:
 sysctl --system
 
-
-# Install containerd:
-apt-get update && sudo apt-get install -y containerd
-
-# Create default configuration file for containerd:
-mkdir -p /etc/containerd
-
-# Generate default containerd configuration and save to the newly created default file:
+#Install and configure containerd 
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable" -y
+sudo apt update -y
+sudo apt install -y containerd.io
+sudo mkdir -p /etc/containerd
 containerd config default | sudo tee /etc/containerd/config.toml
 
-# Restart containerd to ensure new configuration file usage:
-systemctl restart containerd
+#Start containerd
+sudo systemctl restart containerd
+sudo systemctl enable containerd
 
-#Verify that containerd is running:
-systemctl status containerd
-
-# Disable swap:
-swapoff -a
-
-# Install dependency packages:
-apt-get update && sudo apt-get install -y apt-transport-https curl
-# Download and add GPG key:
-curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
-# Add Kubernetes to repository list:
-cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list
-deb https://apt.kubernetes.io/ kubernetes-xenial main
-EOF
-# Update package listings:
-apt-get update
-# Install Kubernetes packages (Note: If you get a dpkg lock message, just wait a minute or two before trying the command again):
-apt-get install -y kubelet=1.24.0-00 kubeadm=1.24.0-00 kubectl=1.24.0-00
-# Turn off automatic updates:
-apt-mark hold kubelet kubeadm kubectl
-
-
+sudo kubeadm config images pull --image-repository=registry.k8s.io --cri-socket unix:///run/containerd/containerd.sock --kubernetes-version v1.26.0
 
 #Possible debug step
 #echo 1 > /proc/sys/net/ipv4/ip_forward
@@ -74,24 +67,29 @@ apt-mark hold kubelet kubeadm kubectl
 -------------
 
 # Initialize the Kubernetes cluster on the control plane node using kubeadm (Note: This is only performed on the Control Plane Node):
-kubeadm init --pod-network-cidr 192.168.0.0/16 --kubernetes-version 1.24.0
+sudo kubeadm init --pod-network-cidr=10.244.0.0/16 --kubernetes-version=v1.26.0 --cri-socket unix:///run/containerd/containerd.sock
 
 # Set kubectl access through kube config:
-mkdir -p $HOME/.kube
-cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
-chown $(id -u):$(id -g) $HOME/.kube/config
+sudo mkdir -p $HOME/.kube
+sudo cp -i /etc/kubernetes/admin.conf $HOME/.kube/config
+sudo chown $(id -u):$(id -g) $HOME/.kube/config
+export KUBECONFIG=/etc/kubernetes/admin.conf
 
+# Disable swap:
+swapoff -a
+
+#Install CNI Flannel
+kubectl apply -f https://github.com/coreos/flannel/raw/master/Documentation/kube-flannel.yml
 
 #Test step
 # kubectl get nodes
 
-# On the control plane node, install Calico Networking:
-kubectl apply -f https://docs.projectcalico.org/manifests/calico.yaml
+-------------------------
 
 #Troubleshooting
-systemctl stop kubelet
-systemctl start kubelet
-strace -eopenat kubectl version
+# systemctl stop kubelet
+# systemctl start kubelet
+# strace -eopenat kubectl version
 
 
 # In the control plane node, create the token and copy the kubeadm join command (NOTE:The join command can also be found in the output from kubeadm init command):
@@ -132,6 +130,14 @@ spec:
 EOF
 
 kubectl apply -f /home/ubuntu/nginx-deployment.yaml
+
+#Update the yaml file to scale the deployment to 4 nginx containers
+#To scale a deployment to 4 containers, you will need to update the replicas field in the deployment configuration file.
+
+
+#Verify the change via the command line
+kubectl get pods -l app=nginx
+
 
 cat << EOF >> /home/ubuntu/multi-container-pod.yaml
 apiVersion: v1
